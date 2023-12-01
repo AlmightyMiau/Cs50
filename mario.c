5e4354535a026a309f13a26d240d847708c5c27e
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2023-12-01 06:30:18 +0000

    Thu, Nov 30, 2023, 10:30 PM -08:00

--- mario.c
+++ mario.c
@@ -3,12 +3,13 @@
 
 int main(void)
 {
+    //Get Size of Grid
     int n;
-    do
-    {
+    do {
         n = get_int("Size: ");
-    }
-    while (n < 1);
+    } while (n < 1);
+
+    //Print Grid
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             printf("#");
