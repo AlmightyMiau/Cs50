807b0c6741857cd85b8ffb5a10781e1e5ef5012b
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2023-12-01 06:43:48 +0000

    Thu, Nov 30, 2023, 10:43 PM -08:00

--- mario.c
+++ mario.c
@@ -1,19 +1,39 @@
 #include <stdio.h>
 #include <cs50.h>
 
+int get_size(void);
+void print_grid(int size);
+
 int main(void)
 {
     //Get Size of Grid
-    int n;
-    do {
-        n = get_int("Size: ");
-    } while (n < 1);
-
+    int size = get_size();
     //Print Grid
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    print_grid(size);
+}
+
+int get_size(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Size: ");
+    }
+    while (n < 1);
+    return n;
+}
+
+void print_grid(size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
             printf("#");
         }
         printf("\n");
     }
 }
+
+
+
