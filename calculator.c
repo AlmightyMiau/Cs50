b10bc0499111388d13dbc0926f425edc01ba3653
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2023-12-01 06:53:38 +0000

    Thu, Nov 30, 2023, 10:53 PM -08:00

--- calculator.c
+++ calculator.c
@@ -6,6 +6,6 @@ int main(void)
     long x = get_int("x: ");
     long y = get_int("y: ");
 
-    float z = (float) x / (float) y;
+    double z = (double) x / (double) y;
     printf("%.20f\n", z);
 }
