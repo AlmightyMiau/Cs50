0f1f894ebe99708b34874b032f7c94e494dd2a58
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2023-12-01 05:57:18 +0000

    Thu, Nov 30, 2023, 9:57 PM -08:00

--- meow.c
+++ meow.c
@@ -9,3 +9,27 @@ int main(void)
         printf("meow\n");
     }
 }
+
+   return 0;
+}
+
+   return 0;
+}
+
+When the above code is compiled and executed, it produces the following result −
+
+value of a: 10
+value of a: 11
+value of a: 12
+value of a: 13
+value of a: 14
+value of a: 15
+value of a: 16
+value of a: 17
+value of a: 18
+value of a: 19
+
+Kickstart Your Career
+
+Get certified by completing the course
+Get Started
