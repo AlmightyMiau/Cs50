74740e62bc80dc177280a69d4c743ad2d9845883
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2023-12-03 09:57:23 +0000

    Sun, Dec 3, 2023, 1:57 AM -08:00

--- wk1/pset1/mario-more/mario.c
+++ wk1/pset1/mario-more/mario.c
@@ -1,7 +1,39 @@
-#include <cs50.h>
 #include <stdio.h>
+#include <cs50.h>
 
-int main(void) 
+int get_size(void);
+void print_grid(int size);
+
+int main(void)
 {
-    
+    //Get Size of Grid
+    int size = get_size();
+    //Print Grid
+    print_grid(size);
 }
+
+int get_size(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Size: ");
+    }
+    while (n < 1);
+    return n;
+}
+
+void print_grid(size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            printf("#");
+        } 
+        printf("\n");
+    }
+}
+
+
+
