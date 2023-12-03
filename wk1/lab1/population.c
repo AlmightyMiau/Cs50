fa12c4a9a89eb1b8e6db1e84dcff7411634352c2
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2023-12-03 06:43:04 +0000

    Sat, Dec 2, 2023, 10:43 PM -08:00

--- wk1/lab1/population.c
+++ wk1/lab1/population.c
@@ -1,24 +1,24 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 
 int main(void)
 {
     float n;
     do
     {
-    n = get_int("Start size: ");
+        n = get_int("Start size: ");
     }
     while (n < 9);
     int end;
     do
     {
-    end = get_int("End size: ");
+        end = get_int("End size: ");
     }
     while (end <= n);
     int year = 0;
     while (n < end)
     {
-        n += (n/12);
+        n += (n / 12);
         year++;
     }
     printf("Years: %i\n", year);
