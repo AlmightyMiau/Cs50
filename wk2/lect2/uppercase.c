cf87d5fad0f55d7742417dde1694fc61eaa6564a
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2023-12-05 05:01:44 +0000

    Mon, Dec 4, 2023, 9:01 PM -08:00

--- wk2/lect2/uppercase.c
+++ wk2/lect2/uppercase.c
@@ -6,5 +6,16 @@ int main(void)
 {
     string s = get_string("Before: ");
     printf("After: ");
-    for (int i = 0; i < strlen)
+    for (int i = 0; i < strlen(s); i++)
+    {
+        if (s[i] >= 'a' && s[i] <= 'z')
+        {
+            printf("%c", s[i] - 32);
+        }
+        else
+        {
+            printf("%c", s[i]);
+        }
+    }
+    printf("\n");
 }
