d1a1a5a9205d709874890a06c2197d7154a64b91
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2023-12-05 05:14:28 +0000

    Mon, Dec 4, 2023, 9:14 PM -08:00

--- wk2/lect2/greet.c
+++ wk2/lect2/greet.c
@@ -3,7 +3,7 @@
 
 //argc is argument count, ie how many arguments were given
 //argv is an array of the arguments given
-//argv[0] is the 
+//argv[0] is the command that calls the script, ie ./greet
 int main(int argc, string argv[])
 {
     printf("hello, %s\n", argv[1]);
