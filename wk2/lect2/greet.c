372310563f11e10d868d914b5c767ea1e1acf4d6
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2023-12-05 05:13:13 +0000

    Mon, Dec 4, 2023, 9:13 PM -08:00

--- wk2/lect2/greet.c
+++ wk2/lect2/greet.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <cs50.h>
 
+//argc is argument count, ie how many arguments were given
+//argv is an array of the arguments given
+//argv[0] is the 
 int main(int argc, string argv[])
 {
     printf("hello, %s\n", argv[1]);
