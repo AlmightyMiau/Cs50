507084244f4809641759293ac549d8707ffde47b
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2023-12-06 05:23:15 +0000

    Tue, Dec 5, 2023, 9:23 PM -08:00

---
+++ wk2/sect2/cla.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <cs50.h>
+#include <string.h>
+
+int main(int argc, string argv[])
+{
+    for (int i = 0; i < argc; i++)
+    {
+        printf("argv[%i] is %s\n", i, argv[i]);
+    }
+}
