ebbaa478f3a5e2ad3f4e33b1f8eff5c7e8b89a4d
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2023-12-06 05:08:15 +0000

    Tue, Dec 5, 2023, 9:08 PM -08:00

--- wk2/sect2/alpha.c
+++ wk2/sect2/alpha.c
@@ -7,14 +7,16 @@ int main(void)
 {
     string word = get_string("Word: ");
     int l = strlen(word);
+    int k = 0;
     for (int i = 0; i < l; i++)
     {
         if (((word[i] > 96 && word[i] < 123) && (word[i - 1] > 96 && word[i - 1] < 123) && (word[i] > word[i - 1])))
         {
-            int a = word[i];
-            word[i] = word[i - 1];
-            word[i - 1] = a;
+
         }
     }
-    printf("%s\n", word);
+    if ()
+    {
+        printf("%s\n", word);
+    }
 }
