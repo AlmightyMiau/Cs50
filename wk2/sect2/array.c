b88da46eace37517325e9f7890a561d8af5e7e41
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2023-12-06 04:41:31 +0000

    Tue, Dec 5, 2023, 8:41 PM -08:00

--- wk2/sect2/array.c
+++ wk2/sect2/array.c
@@ -11,8 +11,7 @@ int main(void)
     while (n < 1);
     int array[n];
     array[0] = 1;
-    int i = 1;
-    do
+    for (int i = 0; i < n; )
     {
         array[i] = array[i - 1] * 2;
         i++;
