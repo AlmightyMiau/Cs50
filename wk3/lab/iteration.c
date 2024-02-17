19fd1f6fe17e43ebd83020539a13af18197070cc
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2024-02-17 23:58:48 +0000

    Sat, Feb 17, 2024, 3:58 PM -08:00

--- wk3/lab/iteration.c
+++ wk3/lab/iteration.c
@@ -14,7 +14,7 @@ int main(void) {
 void draw(int n) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < i + 1; j++) {
-            print("#");
+            printf("#");
         }
         printf("\n");
     }
