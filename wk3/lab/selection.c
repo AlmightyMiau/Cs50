71d4187657bd06f03a6111f66e9f2dd4ec7d1712
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2024-02-17 23:27:18 +0000

    Sat, Feb 17, 2024, 3:27 PM -08:00

--- wk3/lab/selection.c
+++ wk3/lab/selection.c
@@ -5,5 +5,28 @@
 #include <string.h>
 
 int main(void) {
-    
+    int arr[] = 
+    for (int i = 0; i < arr.length - 1; i++) {
+        int n;
+        int m;
+        for (int j = i; j < arr.length - 1; j++) {
+            if (arr[j] < n || n === undefined) {
+                n = arr[j];
+                m = j;
+            }
+        }
+        int k = arr[i];
+        arr[i] = n;
+        arr[m] = k;
+    }
 }
+
+
+/*
+    grab the first value and record it
+    go throught each value and record the smallest
+    swap the 1st value and the smallest value
+    repeat
+
+
+*/
