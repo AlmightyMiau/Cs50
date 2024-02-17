8c2a6725d4dd95398da287d54bde6b4ef08f8688
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2024-02-17 23:32:41 +0000

    Sat, Feb 17, 2024, 3:32 PM -08:00

---
+++ wk3/lab/bubble.c
@@ -0,0 +1,19 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+
+}
+
+
+/*
+    look at each pair of numbers i & i +1
+    if they're out of order swap them
+    repeat until completely sorted
+
+    worst sorting method
+    O(n)
+*/
