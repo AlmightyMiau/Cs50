8b4d0e991004f55d53363a77dd0670b05f0f4e1f
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2024-02-18 06:41:50 +0000

    Sat, Feb 17, 2024, 10:41 PM -08:00

---
+++ wk3/sect/structs.c
@@ -0,0 +1,19 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {}
+
+
+/*
+typedef struct {
+    string name;
+    int votes;
+} candidate;
+
+candidate president;
+president.name = "Alyssa";
+president.votes = 10;
+*/
