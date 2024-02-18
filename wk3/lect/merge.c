82951e9a512bdfd8b76ac1f7680e9ea79aaa58a5
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2024-02-18 00:13:06 +0000

    Sat, Feb 17, 2024, 4:13 PM -08:00

---
+++ wk3/lect/merge.c
@@ -0,0 +1,18 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+    
+}
+
+/*
+    If only one number
+        quit
+    Else
+        Sort left half
+        Sort right half
+        Merge sorted halves
+*/
