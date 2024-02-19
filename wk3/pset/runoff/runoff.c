8ae2f3a923a4a4b9c02daf665d9953a21191ccb2
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2024-02-19 21:43:05 +0000

    Mon, Feb 19, 2024, 1:43 PM -08:00

--- wk3/pset/runoff/runoff.c
+++ wk3/pset/runoff/runoff.c
@@ -164,23 +164,15 @@ void tabulate(void)
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
-    int f = 0;
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].votes >= (voter_count / 2))
+        if (candidates[i].votes > (voter_count / 2))
         {
             printf("%s", candidate.name);
-            f++;
         }
     }
-    if (f > 0)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
+    return false;
+
 }
 
 // Return the minimum number of votes any remaining candidate has
