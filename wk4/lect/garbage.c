d3975d75be5830cc8efc357d5d70f9b5af3c38a8
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2024-02-27 21:12:33 +0000

    Tue, Feb 27, 2024, 1:12 PM -08:00

---
+++ wk4/lect/garbage.c
@@ -0,0 +1,14 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+    int scores[1024];
+    for (int i = 0; i < 1024; i++)
+    {
+        printf("$i\n", scores[i]);
+    }
+}
