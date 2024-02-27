a5b1a43bfc2887994604b494ff2e48f028485439
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2024-02-27 20:38:40 +0000

    Tue, Feb 27, 2024, 12:38 PM -08:00

---
+++ wk4/lect/copy.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void)
+{
+    string s = get_string("s: ");
+
+    string t = s;
+
+    t[0] = toupper(t[0]);
+
+    printf("%s\n", s);
+    printf("%s\n", t);
+}
