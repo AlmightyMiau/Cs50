4eed4f3e08fbdaf4dd2d4d4293a82b0df2a9d0e6
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2024-04-04 06:58:52 +0000

    Wed, Apr 3, 2024, 11:58 PM -07:00

--- wk4/pset/recover/recover.c
+++ wk4/pset/recover/recover.c
@@ -3,12 +3,21 @@
 
 int main(int argc, char *argv[])
 {
+    if (argc > 1) {
+        printf("Usage: ./recover infile\n");
+        return 1;
+    }
+
     // Remember filenames
     char *infile = argv[optind];
 
     // Open input file
     FILE *inptr = fopen(infile, "r");
+    if (inptr == NULL)
+    {
+        printf("Could not open %s.\n", infile);
+        return 1;
+    }
 
-    
-    fread("card.raw", sizeof(RGBTRIPLE), width, inptr);
+    fread(infile, sizeof(RGBTRIPLE), width, inptr);
 }
