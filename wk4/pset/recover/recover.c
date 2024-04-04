9148c2735984bfdb4d480acfe2ca278825e405d4
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2024-04-04 06:52:27 +0000

    Wed, Apr 3, 2024, 11:52 PM -07:00

--- wk4/pset/recover/recover.c
+++ wk4/pset/recover/recover.c
@@ -3,5 +3,12 @@
 
 int main(int argc, char *argv[])
 {
+    // Remember filenames
+    char *infile = argv[optind];
+
+    // Open input file
+    FILE *inptr = fopen(infile, "r");
+
+    
     fread("card.raw", sizeof(RGBTRIPLE), width, inptr);
 }
