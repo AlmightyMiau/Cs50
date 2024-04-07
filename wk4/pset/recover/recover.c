7e0c08172f6ab1574de497a613eedd789e0daa62
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2024-04-07 23:04:33 +0000

    Sun, Apr 7, 2024, 4:04 PM -07:00

--- wk4/pset/recover/recover.c
+++ wk4/pset/recover/recover.c
@@ -43,7 +43,7 @@ int main(int argc, char *argv[])
             files++
             Open output file with name (file name)
             Copy data from old to new file -->
-            
+            fread()
             fwrite()
         }
     }
