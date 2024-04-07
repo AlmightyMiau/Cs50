f4b932c47390231c070b11b88cc1b41cb5ea25d3
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2024-04-07 23:50:48 +0000

    Sun, Apr 7, 2024, 4:50 PM -07:00

---
+++ wk4/pset/recover/test.c
@@ -0,0 +1,11 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int main(int argc, char *argv[])
+{
+    printf("%f", ceil(sizeof(500000)/512.0) )
+}
