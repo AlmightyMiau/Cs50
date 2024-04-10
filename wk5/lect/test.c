986ef9079edd038f0353e81ea0c4218027392369
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2024-04-10 20:49:36 +0000

    Wed, Apr 10, 2024, 1:49 PM -07:00

---
+++ wk5/lect/test.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+int main(void)
+{
+    int i = 000;
+    printf("%i", i);
+}
