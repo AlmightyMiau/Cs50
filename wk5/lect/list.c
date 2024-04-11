6483771e4a782c8922fefcabfe74afaba4fcf3ae
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2024-04-11 03:11:48 +0000

    Wed, Apr 10, 2024, 8:11 PM -07:00

--- wk5/lect/list.c
+++ wk5/lect/list.c
@@ -20,6 +20,17 @@ int main(void)
         return 1;
     }
 
+    for (int i  0; i < 3; i++)
+    {
+        tmp[i] = list[i];
+    }
+    tmp[3] = 4;
+
+
+
+    free(list);
+    list = tmp;
+
     for (int i = 0; i < 3; i++)
     {
         printf("%i\n", list[i]);
