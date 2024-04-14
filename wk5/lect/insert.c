fcc3f3d59c9ce190dfeffe6c22c604bd9d68ea32
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2024-04-14 04:22:10 +0000

    Sat, Apr 13, 2024, 9:22 PM -07:00

--- wk5/lect/insert.c
+++ wk5/lect/insert.c
@@ -1,10 +1,10 @@
 #include <cs50.h>
 #include <ctype.h>
+#include <math.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
 
 typedef struct node
 {
