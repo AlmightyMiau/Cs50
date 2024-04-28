86cf79d95e17191b7c2d47744f1a22259f2d19bc
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2024-04-28 21:16:22 +0000

    Sun, Apr 28, 2024, 2:16 PM -07:00

--- wk5/pset/speller/dictionary.c
+++ wk5/pset/speller/dictionary.c
@@ -22,31 +22,38 @@ node *table[N];
 bool check(const char *word)
 {
     // TODO
+
     return false;
 }
 
 // Hashes word to a number
 unsigned int hash(const char *word)
 {
-    // TODO: Improve this hash function
-    return toupper(word[0]) - 'A';
+    // (sum of (letter - 'A') % 26) of a word to get a value of where to store it in the hash table
+    int val = 0;
+    for (int i = 0; word[i] != '\0'; i++)
+    {
+        val += toupper(word[i]) - 'A';
+    }
+    return val %= 26;
 }
 
-FILE *file = NULL;
 
+// TODO
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
-    // TODO
-    file = fopen(dictionary, "r");
-    if (file != NULL)
-    {
-        return true;
+    // Open dictionary file
+    FILE *source = fopen(dictionary, "r");
+
+    //read each word in dictionary
+    char c;
+    while (fread(&c, 1, 1, source) != 0) {
+
     }
-    else
-    {
-        return false;
-    }
+
+    // Close the dictionary file
+    fclose(source);
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
@@ -54,6 +61,7 @@ unsigned int size(void)
 {
     // TODO
     char c;
+    int n = 0;
     while (fread(&c, 1, 1, file))
     {
         if (c == "\n")
