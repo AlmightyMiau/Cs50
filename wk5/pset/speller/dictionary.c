4c2b530a1f4136139119198eb76a283006da1c99
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2024-04-29 03:26:22 +0000

    Sun, Apr 28, 2024, 8:26 PM -07:00

--- wk5/pset/speller/dictionary.c
+++ wk5/pset/speller/dictionary.c
@@ -48,13 +48,8 @@ bool check(const char *word)
 // Hashes word to a number
 unsigned int hash(const char *word)
 {
-    // (sum of (letter - 'A') % 26) of a word to get a value of where to store it in the hash table
-    int val = 0;
-    for (int i = 0; word[i] != '\0'; i++)
-    {
-        val += toupper(word[i]) - 'A';
-    }
-    return (val % 26);
+    // TODO: Improve this hash function
+    return toupper(word[0]) - 'A';
 }
 
 // Loads dictionary into memory, returning true if successful, else false
