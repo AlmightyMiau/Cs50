9a47911873cbdac819d4b49614e9fb2738fd8a04
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2024-04-29 04:00:56 +0000

    Sun, Apr 28, 2024, 9:00 PM -07:00

--- wk5/pset/speller/dictionary.c
+++ wk5/pset/speller/dictionary.c
@@ -50,9 +50,7 @@ unsigned int hash(const char *word)
     // (sum of (letter - 'A') % 26) of a word to get a value of where to store it in the hash table
     int val = 0;
     int i = 0;
-    char *spill[LENGTH];
-    strcpy(spill, word);
-    while (spill[i] != "/0")
+    while (word[i] != "/0")
     {
         val += (word[i] % 26);
     }
