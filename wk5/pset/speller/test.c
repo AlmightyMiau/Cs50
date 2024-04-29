a664bf7383b41c5f266485454154b516e4d1e164
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2024-04-29 02:13:57 +0000

    Sun, Apr 28, 2024, 7:13 PM -07:00

---
+++ wk5/pset/speller/test.c
@@ -0,0 +1,20 @@
+#include <cs50.h>
+#include <ctype.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+// Hashes word to a number
+int main(void)
+{
+    char *word = "cat";
+    // (sum of (letter - 'A') % 26) of a word to get a value of where to store it in the hash table
+    int val = 0;
+    for (int i = 0; word[i] != '\0'; i++)
+    {
+        val += toupper(word[i]) - 'A';
+    }
+    return val %= 26;
+}
