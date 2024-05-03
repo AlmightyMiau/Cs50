e3f305423ecac8b2cd177ffbf49b59f4b264bea1
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2024-05-03 08:20:29 +0000

    Fri, May 3, 2024, 1:20 AM -07:00

--- wk6/lect/phonebook.py
+++ wk6/lect/phonebook.py
@@ -1,12 +1,28 @@
-names = ["Carter", "David", "Jogn"]
+# names = ["Carter", "David", "Jogn"]
+#
+# name = input("Name: ")
+#
+# # for n in names:
+# #     if name == n:
+# #         print("Found")
+# #         break
+# if name in names:
+#     print("Found")
+# else:
+#     print("Not found")
+
+people = [
+    {"name": "Carter", "number": "+1-617-495-1000"},
+    {"name": "David", "number": "+1-617-495-1000"},
+    {"name": "John", "number": "+1-949-468-2750"},
+]
 
 name = input("Name: ")
 
-# for n in names:
-#     if name == n:
-#         print("Found")
-#         break
-if name in names:
-    print("Found")
+for person in people:
+    if person["name"] == name:
+        number = person["number"]
+        print("Found", number)
+        break
 else:
     print("Not found")
