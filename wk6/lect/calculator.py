728d38700ae6746906a2784306dda6ec4980ecc2
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2024-05-03 07:47:20 +0000

    Fri, May 3, 2024, 12:47 AM -07:00

--- wk6/lect/calculator.py
+++ wk6/lect/calculator.py
@@ -1,6 +1,12 @@
-import cs50
+# import cs50
 
-x = cs50.get_int("x: ")
-y = cs50.get_int("y: ")
+# x = cs50.get_int("x: ")
+# y = cs50.get_int("y: ")
 
-print(x + y)
+# print(x + y)
+
+x = int(input("x: "))
+y = int(input("y: "))
+
+z = x / y
+print(z)
