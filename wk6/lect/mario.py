10a7bd53241ea4fbe959d7b5b0f97b21a4cc285b
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2024-05-03 08:05:19 +0000

    Fri, May 3, 2024, 1:05 AM -07:00

---
+++ wk6/lect/mario.py
@@ -0,0 +1,22 @@
+# from cs50 import get_int
+#
+# while True:
+#     n = get_int("Width: ")
+#     if n > 0:
+#         break
+#
+# # for i in range(n):
+# #     print("#", end="")
+# #
+# # print()
+#
+# print("#" * n)
+
+
+# for i in range(3):
+#     for j in range(3):
+#         print("#", end="")
+#     print()
+
+for i in range(3):
+    print("#" * 3)
