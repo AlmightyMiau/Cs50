5782df7353475af2de922918913ffae4d0862f2f
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2024-05-04 07:19:44 +0000

    Sat, May 4, 2024, 12:19 AM -07:00

--- wk6/pset/sentimental-mario-more/mario.py
+++ wk6/pset/sentimental-mario-more/mario.py
@@ -2,4 +2,16 @@
 # get input using get_int
 # make sure the input is between 1 and 8
 # print the pyramids
-# 
+# for i in range(input - 1):
+    # print(" " * (input - i - 1), "#" * i + 1)
+
+from cs50 import get_int
+
+size = 0
+
+while size < 1 && size > 8:
+    size = get_int("Height: ")
+
+for i in range(input - 1):
+    print(" " * (input - i - 1), "#" * i + 1)
+
