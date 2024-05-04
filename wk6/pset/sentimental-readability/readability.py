b33b562061be70dd41e53f7f3934265c17481bf7
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2024-05-04 23:04:25 +0000

    Sat, May 4, 2024, 4:04 PM -07:00

--- wk6/pset/sentimental-readability/readability.py
+++ wk6/pset/sentimental-readability/readability.py
@@ -5,6 +5,7 @@ text = get_string("Text: ")
 s = 0.0 # track how many sentences there are
 w = 1.0 # track how many words there are
 l = 0.0 # letters
-i = 0
 c = ''
 
+for i in range(len(text)):
+    c = text[i]
