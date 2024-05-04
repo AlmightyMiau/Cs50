1c5793897dab16dab930263187b0e5989c040ed7
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2024-05-04 08:05:19 +0000

    Sat, May 4, 2024, 1:05 AM -07:00

--- wk6/pset/sentimental-cash/cash.py
+++ wk6/pset/sentimental-cash/cash.py
@@ -5,17 +5,31 @@ while True:
     if cents > 0:
         break
 
+print(cents)
+
+cents = round(cents, 2)
 quarters = cents // 0.25
 cents -= quarters * 0.25
+cents = round(cents, 2)
+print(cents)
 
+cents = round(cents, 2)
 dimes = cents // 0.10
 cents -= dimes * 0.10
+cents = round(cents, 2)
+print(cents)
 
+cents = round(cents, 2)
 nickels = cents // 0.05
 cents -= nickels * 0.05
+cents = round(cents, 2)
+print(cents)
 
+cents = round(cents, 2)
 pennies = cents // 0.01
 cents -= pennies * 0.01
+cents = round(cents, 2)
+print(cents)
 
 coins = quarters + dimes + nickels + pennies
 print(int(coins))
