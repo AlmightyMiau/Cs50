d2b589544de89731ce2518e26f3d9853956413a5
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2024-05-04 07:58:37 +0000

    Sat, May 4, 2024, 12:58 AM -07:00

--- wk6/pset/sentimental-cash/cash.py
+++ wk6/pset/sentimental-cash/cash.py
@@ -1,9 +1,21 @@
 from cs50 import get_float
 
 while True:
-    cents = get_float("Chnage: ")
+    cents = get_float("Change: ")
     if cents > 0:
         break
 
-quarters = cents / 25
-cents =
+quarters = cents // 25
+cents -= quarters * 0.25
+
+dimes = cents // 10
+cents -= dimes * 0.10
+
+nickels = cents // 5
+cents -= nickels * 0.05
+
+pennies = cents // 1
+cents -= pennies * 0.01
+
+coins = quarters + dimes + nickels + pennies
+print(coins)
