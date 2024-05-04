d3dbd981a68173947d538d19eb1e82bf2b67599e
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2024-05-04 08:07:39 +0000

    Sat, May 4, 2024, 1:07 AM -07:00

--- wk6/pset/sentimental-cash/cash.py
+++ wk6/pset/sentimental-cash/cash.py
@@ -7,25 +7,21 @@ while True:
 
 print(cents)
 
-cents = round(cents, 2)
 quarters = cents // 0.25
 cents -= quarters * 0.25
 cents = round(cents, 2)
 print(cents)
 
-cents = round(cents, 2)
 dimes = cents // 0.10
 cents -= dimes * 0.10
 cents = round(cents, 2)
 print(cents)
 
-cents = round(cents, 2)
 nickels = cents // 0.05
 cents -= nickels * 0.05
 cents = round(cents, 2)
 print(cents)
 
-cents = round(cents, 2)
 pennies = cents // 0.01
 cents -= pennies * 0.01
 cents = round(cents, 2)
