1e87f1deabec697330bd590c39ad85b65d188e74
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2024-05-04 23:02:04 +0000

    Sat, May 4, 2024, 4:02 PM -07:00

--- wk6/pset/sentimental-readability/readability.py
+++ wk6/pset/sentimental-readability/readability.py
@@ -2,3 +2,9 @@ from cs50 import get_string
 
 text = get_string("Text: ")
 
+s = 0.0 # track how many sentences there are
+w = 1.0 # track how many words there are
+l = 0.0 # letters
+i = 0
+c = ''
+
