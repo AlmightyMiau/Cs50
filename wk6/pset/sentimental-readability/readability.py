5121d368936d3d1f2611636e9183c060be7f3bf5
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2024-05-04 23:31:54 +0000

    Sat, May 4, 2024, 4:31 PM -07:00

--- wk6/pset/sentimental-readability/readability.py
+++ wk6/pset/sentimental-readability/readability.py
@@ -2,9 +2,9 @@ from cs50 import get_string
 
 text = get_string("Text: ")
 
-s = 0.0 # sentences
-w = 1.0 # words
-l = 0.0 # letters
+s = 0.0  # sentences
+w = 1.0  # words
+l = 0.0  # letters
 c = ''
 
 for i in range(len(text)):
