ce0c71df823cc5d3ebe4f4b6b55a8109bb9a5fc9
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2024-05-20 09:20:34 +0000

    Mon, May 20, 2024, 2:20 AM -07:00

--- wk7/lect/favorites.py
+++ wk7/lect/favorites.py
@@ -15,6 +15,5 @@ with open("favorites.csv") as file:
         favorite = row["language"]
         counts[favorite] += 1
 
-for favorite in sorted(counts, key=counts.get, reverse=True):
-    print(favorite, ':', counts[favorite])
-
+for favorite, count in counts.most_common():
+    print(f"{favorite}: {count}")
