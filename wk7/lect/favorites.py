001ce22afb5fba04e51c748ae680d0d34baaded2
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2024-05-20 09:02:49 +0000

    Mon, May 20, 2024, 2:02 AM -07:00

--- wk7/lect/favorites.py
+++ wk7/lect/favorites.py
@@ -1,7 +1,13 @@
+
+# DOESN'T WORK BECAUSE WE DON'T HAVE FAVORITES.CSV
+
+
+
 import csv
 
 with open("favorites.csv") as file:
     reader = csv.reader(file)
     next(reader)
     for row in reader:
+        favorite = row[1]
         print(row[1])
