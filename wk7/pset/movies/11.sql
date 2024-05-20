5d7a128204108ca96b6a35a4c43ed01de24b0bb8
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2024-05-20 11:28:00 +0000

    Mon, May 20, 2024, 4:28 AM -07:00

--- wk7/pset/movies/11.sql
+++ wk7/pset/movies/11.sql
@@ -1,10 +1,14 @@
 SELECT title
 FROM movies
-JOIN ratings ON movies.id = ratings.movie_id
-WHERE person_id = (
-    SELECT id
-    FROM people
-    WHERE name = "Chadwick Boseman"
+-- JOIN ratings ON movies.id = ratings.movie_id
+WHERE id IN (
+    SELECT movie_id
+    FROM ratings
+    WHERE person_id = (
+        SELECT id
+        FROM people
+        WHERE name = "Chadwick Boseman"
+    )
 )
 ORDER BY rating
 ;
