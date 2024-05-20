12d30dbe343adbe6a14051d73a91c9cebd956f88
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2024-05-20 11:38:26 +0000

    Mon, May 20, 2024, 4:38 AM -07:00

--- wk7/pset/movies/13.sql
+++ wk7/pset/movies/13.sql
@@ -1,20 +1,21 @@
 SELECT name
 FROM people
 WHERE id IN (
-    SELECT movie_id
+    SELECT person_id
     FROM stars
-    WHERE person_id = (
+    WHERE movie_id = (
         SELECT id
-        FROM people
-        WHERE name = "Bradley Cooper"
-    )
-) AND (
-    SELECT movie_id
-    FROM stars
-    WHERE person_id = (
-        SELECT id
-        FROM people
-        WHERE name = "Jennifer Lawrence"
+        FROM movies
+        WHERE id IN (
+            SELECT movie_id
+            FROM stars
+            WHERE person_id = (
+                SELECT id
+                FROM people
+                WHERE name = "Kevin Bacon"
+                AND year = 1958
+            )
+        )
     )
 )
 ;
