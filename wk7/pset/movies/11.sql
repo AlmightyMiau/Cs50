e419d486012c51895ff73cbeb9a309d6b9a1255c
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2024-05-20 11:29:42 +0000

    Mon, May 20, 2024, 4:29 AM -07:00

--- wk7/pset/movies/11.sql
+++ wk7/pset/movies/11.sql
@@ -1,9 +1,9 @@
 SELECT title
 FROM movies
--- JOIN ratings ON movies.id = ratings.movie_id
+JOIN ratings ON movies.id = ratings.movie_id
 WHERE id IN (
     SELECT movie_id
-    FROM ratings
+    FROM stars
     WHERE person_id = (
         SELECT id
         FROM people
