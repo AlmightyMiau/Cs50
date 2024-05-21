be8633cd3aa2aa4b587906d5c0630696fcd2f4e6
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2024-05-21 06:48:36 +0000

    Mon, May 20, 2024, 11:48 PM -07:00

--- wk7/pset/fiftyville/log.sql
+++ wk7/pset/fiftyville/log.sql
@@ -1,2 +1,15 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+-- Find out what tables I can use to find information
+.tables
+
+-- What can I find in the crime_scene_reports table?
+.schema crime_scene_reports
+
+-- Find the report from July 28, 2023 and on Humphrey Street
+SELECT description
+FROM crime_scene_reports
+WHERE year = 2023
+AND month = 7
+AND day = 28
+AND street = "Humphrey Street";
