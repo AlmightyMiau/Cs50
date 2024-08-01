090a2ed3ce9d1ff69063d43359d2d79448d0e28d
Fudgerboy <91767657+Fudgerboy@users.noreply.github.com>
2024-08-01 09:45:10 +0000

    Thu, Aug 1, 2024, 2:45 AM -07:00

--- wk9/lect/cookies/app.py
+++ wk9/lect/cookies/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, session
+from flask_session import Session
 
 app = Flask(__name__)
 
@@ -8,11 +9,12 @@ Session(app)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", name=session.get("name"))
 
 
 @app.route("/login")
 def login():
     if request.method == "POST":
-        session
+        session["name"] = request.form.get("name")
+        return redirect("/")
     return render_template("login.html")
